e24a6f9e631bd63dae55edf1a8d67a31d6c24cc8
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-08-08 11:23:13 -0300

    template and schema

---
+++ index.js
@@ -0,0 +1,3 @@
+import server from './src/index'
+
+server.start(() => console.log('Listening in port 4000'))
---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "SkillType",
+    embedded: false
+  },
+  {
+    name: "Skill",
+    embedded: false
+  },
+  {
+    name: "Trade",
+    embedded: false
+  },
+  {
+    name: "Rating",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/victor-cosenza-b8ed0c/graphql-backend/dev`
+});
+exports.prisma = new exports.Prisma();
